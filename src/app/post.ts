eac1f9e57e3dd53cb39c19e6308ef8fdb18f729b
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2018-10-20 17:53:45 +0800

    Add dates

--- src/app/post.ts
+++ src/app/post.ts
@@ -2,4 +2,6 @@ export class Post {
     id: number;
     title: string;
     revisions: number;
+    creation_time: Date;
+    modification_time: Date;
 }
--- src/app/wordpress-metadata-service.service.ts
+++ src/app/wordpress-metadata-service.service.ts
@@ -12,11 +12,23 @@ export class WordpressMetadataServiceService {
 
     constructor(private http: HttpClient) { }
 
+    getDate(wpDateStr: string): Date {
+        let datetime = wpDateStr.split('T');
+        let date = datetime[0].split('-');
+        let time = datetime[1].split(':');
+
+        return new Date(Date.UTC(
+            parseInt(date[0]), parseInt(date[1]), parseInt(date[2]), parseInt(time[0]), parseInt(time[1]), parseInt(time[2])
+        ));
+    }
+
     getPosts(): Observable<Post[]> {
         return this.http.get<any[]>("https://blog.ahyangyi.org/wp-json/wp/v2/posts").pipe(map(x => x.map(y => {return {
             id: y.id,
             title: y.title.rendered,
             revisions: y._links['version-history'][0].count,
+            creation_time: this.getDate(y.date_gmt),
+            modification_time: this.getDate(y.modified_gmt),
             };})));
     }
 }
