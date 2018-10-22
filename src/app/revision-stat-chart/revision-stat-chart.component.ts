ddfe9ca70c6ac6721c794da01da4416b876a9127
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2018-10-22 11:15:40 +0800

    Set a color scheme

--- src/app/revision-stat-chart/revision-stat-chart.component.ts
+++ src/app/revision-stat-chart/revision-stat-chart.component.ts
@@ -10,6 +10,22 @@ export class RevisionStatChartComponent implements OnInit, OnChanges {
     @Input() data: Post[];
 
     results: any;
+    scheme = {
+        'domain': [
+            '#63B2FF', // A400
+            '#165A94', // 900
+            '#96CBFF', // A200
+            '#378ABA', // 500
+            '#C3DCEA', // 100
+            '#49A6FF', // A700
+            '#559CC4', // 400
+            '#236DA3', // 800
+            '#73ADCF', // 300
+            '#2A77AB', // 700
+            '#9BC5DD', // 200
+            '#3182B3', // 600
+        ],
+        }
 
     constructor() { }
 
