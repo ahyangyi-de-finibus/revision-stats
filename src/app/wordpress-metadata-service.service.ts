d2ed52cb501caf8bad0d301991892d128c78470e
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2019-02-03 16:03:12 +0800

    Fix escaped titles

--- src/app/wordpress-metadata-service.service.ts
+++ src/app/wordpress-metadata-service.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Post } from './post';
+import * as he from 'he';
 
 @Injectable({
     providedIn: 'root'
@@ -25,7 +26,7 @@ export class WordpressMetadataServiceService {
     getPosts(): Observable<Post[]> {
         return this.http.get<any[]>("https://blog.ahyangyi.org/wp-json/wp/v2/posts?per_page=100").pipe(map(x => x.map(y => {return {
             id: y.id,
-            title: y.title.rendered,
+            title: he.decode(y.title.rendered),
             revisions: y._links['version-history'][0].count,
             creation_time: this.getDate(y.date_gmt),
             modification_time: this.getDate(y.modified_gmt),
